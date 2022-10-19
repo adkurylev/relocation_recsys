1125c2c5b960ebb3b4c04c64370675b2229be251
PhySci <mushenokf@gmail.com>
2022-10-19 22:32:49 +0300

    Basis of Tg bot

--- src/tg/app/app.py
+++ src/tg/app/app.py
@@ -10,8 +10,11 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from utils import setup_logging
+from settings import BOT_TOKEN
 
 _logger = logging.getLogger(__name__)
+action_clb = CallbackData('action_callback', 'action')
+
 
 def init_bot():
     bot = Bot(token=BOT_TOKEN)
@@ -24,7 +27,35 @@ bot, dp = init_bot()
 
 @dp.message_handler(commands=["start"])
 async def start(message: Message):
-    start_keyboard = InlineKeyboardMarkup()
+    """
+    Start message
+    """
+    # @TODO: replace on db request
+    keyboard = InlineKeyboardMarkup()
+    is_existing_user = False
+    if not is_existing_user:
+        keyboard.add(InlineKeyboardButton(text="Зарегистрироваться",
+                                          callback_data=action_clb.new(action="register")))
+        msg = "Вступительное слово и описание функционала здесь"
+    else:
+        #keyboard.add(InlineKeyboardButton(text="Посмотреть рекомендации",
+        #                                  callback_data=action_clb.new(action="get_rec")))
+        msg = "Вот что я могу тебе предложить"
+
+    keyboard.add(InlineKeyboardButton(text="Посмотреть рекомендации",
+                                      callback_data=action_clb.new(action="get_recommendation")))
+    await message.answer(msg, reply_markup=keyboard)
+
+
+@dp.callback_query_handler(action_clb.filter(action="register"))
+async def register(query: CallbackQuery):
+    # @TODO: post user request
+    await bot.send_message(query.from_user.id, text="Вы успешно зарегистрированы")
+
+
+@dp.callback_query_handler(action_clb.filter(action="get_recommendation"))
+async def show_city(query: CallbackQuery):
+    await bot.send_message(query.from_user.id, text="Щас что-нибудь поищу")
 
 
 async def on_shutdown(dp):
@@ -39,7 +70,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
